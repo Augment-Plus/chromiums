1cf452d90e576d69e35e7bf9b6e5ee3245178bb8
Augment Chinedu <augmentchinedu@gmail.com>
2025-08-17 21:08:10 +0000

    Setup for Puppeteer

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const express = require('express');
-const puppeteer = require('puppeteer-core');
+const express = require("express");
+const puppeteer = require("puppeteer");
 
 const app = express();
 
-app.get('/', async (req, res) => {
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser',
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-gpu',
-            '--disable-dev-shm-usage',
-            '--single-process'
-        ]
-    });
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch({
+    headless: process.env.NODE_ENV !== "production",
+  });
 
-    const page = await browser.newPage();
-    await page.goto('https://example.com');
-    const title = await page.title();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const title = await page.title();
 
-    await browser.close();
-    res.send(`Page title: ${title}`);
+  await browser.close();
+  res.send(`Page title: ${title}`);
 });
 
 const PORT = process.env.PORT || 8080;
